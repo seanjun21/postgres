7bcd8e47289ef19dfc0590f36c6e776a79bf4925
alexbang <alexbang@hotmail.com>
2016-07-08 20:24:09 +0000

    Add tags and pizzas

--- schema.sql
+++ schema.sql
@@ -4,3 +4,28 @@ create table if not exists recipes (
     description text not null
 );
 
+
+create table if not exists tags (
+    id serial primary key,
+    name text not null
+);
+
+-- Common convention: Association tables are named with the two tables they join.
+create table if not exists recipe_tags (
+    recipe_id integer references recipes,
+    tag_id integer references tags,
+    primary key (recipe_id, tag_id)
+);
+
+
+
+create table if not exists pizzas (
+    id serial primary key,
+    name text not null
+);
+
+create table if not exists batches (
+    id serial primary key,
+    pizza_id integer references pizzas,
+    quantity integer not null
+);
